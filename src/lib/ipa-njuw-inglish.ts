785f71bbaac67715f6d12f3db010cbc83d1851c7
Sean Turley <seanmturley@gmail.com>
2025-10-29 18:21:28 +1300

    [#13] Add object that maps IPA symbols to Njuw Iňgliš graphemes

---
+++ src/lib/ipa-njuw-inglish.ts
@@ -0,0 +1,76 @@
+const map = {
+  ///////////////
+  // CONSONANTS
+
+  // nasals
+  // m: "m",
+  // n: "n",
+  ŋ: "ň",
+
+  // plosives (fortis)
+  // p: "p",
+  // t: "t",
+  tʃ: "č",
+  k: "c",
+
+  // plosives (lenis)
+  // b: "b",
+  // d: "d",
+  dʒ: "ǧ",
+  // g: "g",
+
+  // fricatives (fortis)
+  // f: "f",
+  θ: "þ",
+  // s: "s",
+  ʃ: "š",
+  x: "ȟ",
+  ʍ: "w̌",
+  // h: "h",
+
+  // fricatives (lenis)
+  // v: "v",
+  ð: "ð",
+  // z: "z",
+  ʒ: "ž",
+
+  // approximants
+  // l: "l",
+  // r: "r",
+  // j: "j",
+  // w: "w",
+
+  ///////////////
+  // VOWELS
+
+  // short
+  ɪ: "i",
+  e: "é",
+  ɒ: "o",
+  æ: "a",
+  ʊ: "u",
+  ə: "e",
+  ʌ: "á",
+  i: "ij",
+
+  // long
+  ɪə: "ii",
+  ɛː: "éé",
+  ɔː: "oo",
+  ɑː: "aa",
+  ʊə: "uu",
+  ɜː: "ee",
+
+  // j-diphthongs
+  iː: "ij",
+  eɪ: "éj",
+  ɔɪ: "oj",
+  aɪ: "aj",
+
+  // w-diphthings
+  aʊ: "aw",
+  uː: "uw",
+  əʊ: "ew"
+};
+
+export default map;
