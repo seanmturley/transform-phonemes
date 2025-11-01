c1a001b001c4208073efda9cf09bbb3bf864e048
Sean Turley <seanmturley@gmail.com>
2025-11-01 23:07:56 +1300

    [#16] Add an object that maps IPA symbols to West Germanic graphemes

---
+++ src/lib/ipa-west-germanic.ts
@@ -0,0 +1,76 @@
+const map = {
+  ///////////////
+  // CONSONANTS
+
+  // nasals
+  // m: "m",
+  // n: "n",
+  ŋ: "ng",
+
+  // plosives (fortis)
+  // p: "p",
+  // t: "t",
+  tʃ: "c",
+  // k: "k",
+
+  // plosives (lenis)
+  // b: "b",
+  // d: "d",
+  dʒ: "dg",
+  // g: "g",
+
+  // fricatives (fortis)
+  // f: "f",
+  θ: "þ",
+  // s: "s",
+  ʃ: "sh",
+  x: "ch",
+  ʍ: "wh",
+  // h: "h",
+
+  // fricatives (lenis)
+  // v: "v",
+  // ð: "ð",
+  // z: "z",
+  ʒ: "zh",
+
+  // approximants
+  // l: "l",
+  // r: "r",
+  // j: "j",
+  // w: "w",
+
+  ///////////////
+  // VOWELS
+
+  // short
+  ɪ: "i",
+  e: "é",
+  ɒ: "o",
+  æ: "a",
+  ʊ: "u",
+  ə: "e",
+  ʌ: "á",
+  i: "ij",
+
+  // long
+  ɪə: "ii",
+  ɛː: "éé",
+  ɔː: "oo",
+  ɑː: "aa",
+  ʊə: "uu",
+  ɜː: "ee",
+
+  // j-diphthongs
+  iː: "ij",
+  eɪ: "éj",
+  ɔɪ: "oj",
+  aɪ: "aj",
+
+  // w-diphthings
+  aʊ: "aw",
+  uː: "uw",
+  əʊ: "ow"
+};
+
+export default map;
