605ab4e4db747387b740e0f482592be3e570ffa6
Sean Turley <seanmturley@gmail.com>
2025-11-15 21:33:31 +1300

    [#24] Target specific punctuation in vowelOrEndOfClause

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -2,4 +2,4 @@ export const wordStart = "(?<=\\P{L}|^)";
 export const wordEnd = "(?=\\P{L}|$)";
 
 export const vowelOrEndOfClause = (vowels: string) =>
-  `(?=(\\P{L}[${vowels}]|\\P{L}?\\p{P}|$))`;
+  `(?=(\\P{L}[${vowels}]|\\P{L}?[,;.:?!)]|$))`;
