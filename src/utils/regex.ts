ca2c6fafc786c513cbe2e83b8943f2bf82df5d67
Sean Turley <seanmturley@gmail.com>
2025-11-21 22:27:58 +1300

    [#28] Add regex patterns for quotation marks to utils/regex.ts

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -6,3 +6,8 @@ export const followedByVowelOrEndOfClause = (vowels: string) =>
 
 export const followedByConsonant = (consonants: string) =>
   `(?=\\s?[${consonants}])`;
+
+export const primaryQuotes =
+  /(?<=^|\p{L}\p{P}\s|—)['"‘“](.+?)(?:(?:(?<=(?:[\p{L}\d]\p{P}+))['"’”])|['"’”](—))/gmu;
+
+export const secondaryQuotes = /(?<!\p{L})(['"‘“])([^'"‘“]*?)['"’”](?!\p{L})/gu;
