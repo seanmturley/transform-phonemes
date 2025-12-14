2e4b99eb35bb2c9ebf1868b91a8de1d87c47ada8
Sean Turley <seanmturley@gmail.com>
2025-12-14 23:53:34 +1300

    [#31] Add basic map for IPA to Nouvó Fransè Standard

---
+++ src/lib/ipa-nouvo-franse-standard/map.ts
@@ -0,0 +1,93 @@
+import type { TransliterationMap } from "../../transliterate/types.ts";
+
+const map: TransliterationMap = {
+  pre: {
+    // Groups of phonemes are presented in decreasing order of prioritization
+
+    // Phonemes not requiring transliteration are commented out
+
+    exceptions: {},
+
+    trigraphs: {},
+
+    digraphs: {
+      ɛː: "ê"
+    },
+
+    monographs: {
+      ///////////////
+      // CONSONANTS
+
+      // There's currently no provision made for silent final
+      // "liaison consonants"
+
+      // plosives
+      // p: "p",
+      // b: "b",
+      // t: "t",
+      // d: "d",
+      k: "c",
+      // g: "g",
+
+      // fricatives
+      // f: "f",
+      // v: "v",
+      // s: "s",
+      // z: "z",
+      ʃ: "ch",
+      ʒ: "j",
+
+      // nasals
+      // m: "m",
+      // n: "n",
+      ɲ: "gn",
+      ŋ: "ng",
+
+      // approximants
+      // l: "l",
+      // r: "r",
+      j: "ǐ",
+      ɥ: "ǔ",
+      w: "oǔ",
+
+      ///////////////
+      // VOWELS
+
+      // oral
+      i: "i",
+      y: "u",
+      u: "ou",
+      e: "é",
+      ø: "eú",
+      o: "ó",
+      ɛ: "è",
+      œ: "eù",
+      ɔ: "ò",
+      ə: "e",
+      a: "a",
+      ɑ: "â",
+
+      // nasal
+      // It's currently unclear how to handle nasal vowels given their
+      // variable spelling in the current orthographic system
+      ɛ̃: "in", // im, èn, èm
+      œ̃: "un", // um
+      ɔ̃: "òn", // òm
+      ɑ̃: "an" // am, en, em
+    }
+  }
+  // post: {
+  //   always: { ev: "ov", end: "and" },
+  //   beforeConsonants: { intuw: "inte", ontuw: "onte" },
+  //   notBeforeConsonants: { ðe: "ðij", te: "tuw" },
+  //   afterApostrophe: {
+  //     ...ipaLeadingApostrophes
+  //   }
+  // },
+  // regexPatterns: {
+  //   vowels: "AÁEÉIOUaáeéiou",
+  //   consonants: "b-df-hj-np-tv-zðþB-DF-HJ-NP-TV-ZÐÞ"
+  // }
+};
+
+export default map;
